766bd953b60e8e36b986518803947230f788592f
joshoconnor89 <joshoconnor89@gmail.com>
2015-08-21 16:26:53 -0700

    iOS CollectionViewController SwiftLogo
    iOS CollectionViewController SwiftLogo project.  It is written in Swift.

---
+++ IOS8SwiftCustomCollectionViewCellsTutorial/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewController.swift
+//
+//
+//  Created by Kristian Secor on 01/20/15.
+//  Copyright (c) 2015 Kristian Secor. All rights reserved.
+//
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+  @IBOutlet var imageView: UIImageView!
+    
+}
